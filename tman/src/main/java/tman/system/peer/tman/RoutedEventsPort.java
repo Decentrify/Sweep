b8ad280d492be56e34606883d8a215e793e3a2c5
Jim Dowling <jdowling@sics.se>
2013-07-09 11:08:52 +0200

    Small changes

--- tman/src/main/java/tman/system/peer/tman/RoutedEventsPort.java
+++ tman/src/main/java/tman/system/peer/tman/RoutedEventsPort.java
@@ -1,6 +1,7 @@
 package tman.system.peer.tman;
 
 import se.sics.kompics.PortType;
+import se.sics.peersearch.messages.AddIndexEntryMessage;
 import tman.system.peer.tman.LeaderRequest.AddIndexEntry;
 import tman.system.peer.tman.LeaderRequest.GapCheck;
 
@@ -8,6 +9,7 @@ public class RoutedEventsPort extends PortType {
 	{
 		positive(AddIndexEntry.class);
 		negative(AddIndexEntry.class);
+		negative(AddIndexEntryMessage.Request.class);
 		positive(GapCheck.class);
 		negative(GapCheck.class);
 	}
