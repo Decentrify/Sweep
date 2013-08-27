ec5e1acc1196eb543445d15e49f5bb0944fab8b9
Dmitry Kazarin <kazarindn@gmail.com>
2013-08-27 08:29:41 +0200

    Utility function considers NAT type if the peer

--- search/src/main/java/se/sics/ms/search/Search.java
+++ search/src/main/java/se/sics/ms/search/Search.java
@@ -1140,6 +1140,8 @@ public final class Search extends ComponentDefinition {
             minStoredId = getMinStoredIdFromLucene();
             maxStoredId = getMaxStoredIdFromLucene();
 
+            ((MsSelfImpl)self).setNumberOfIndexEntries(Math.abs(maxStoredId - minStoredId + 1));
+
             if(maxStoredId < minStoredId) {
                 long temp = maxStoredId;
                 maxStoredId = minStoredId;
