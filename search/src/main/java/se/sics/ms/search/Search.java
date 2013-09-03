56b5b84693c6a3c5f947414eadde5b3be27b015a
Dmitry Kazarin <kazarindn@gmail.com>
2013-09-03 15:34:58 +0200

    clean up

--- search/src/main/java/se/sics/ms/search/Search.java
+++ search/src/main/java/se/sics/ms/search/Search.java
@@ -114,7 +114,6 @@ public final class Search extends ComponentDefinition {
     private long maxStoredId = Long.MIN_VALUE;
 
     private HashMap<TimeoutId, Long> timeStoringMap = new HashMap<TimeoutId, Long>();
-    private int numOfAdds=0;
 
     private class ExchangeRound extends IndividualTimeout {
 
