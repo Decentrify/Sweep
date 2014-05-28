7051d1c9bec02c8edbc10c55951cbad4bc9c7dfd
Jim Dowling <jdowling@kth.se>
2014-05-29 00:01:43 +0200

    Replaced Intellij GUI with Netbeans Swing GUI

--- search/src/main/java/se/sics/ms/ui/TrayUI.java
+++ search/src/main/java/se/sics/ms/ui/TrayUI.java
@@ -9,14 +9,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 /**
- * Created with IntelliJ IDEA.
- * User: kazarindn
- * Date: 8/7/13
- * Time: 1:05 PM
+ * Created with IntelliJ IDEA. User: kazarindn Date: 8/7/13 Time: 1:05 PM
  */
 public class TrayUI extends TrayIcon implements PropertyChangeListener {
 
@@ -24,9 +23,9 @@ public class TrayUI extends TrayIcon implements PropertyChangeListener {
 
     private final UiComponent component;
     private final JFrame searchFrame;
-    private final SearchUi searchUi;
+    private final SearchSwingUi searchUi;
     private final JFrame addEntryFrame;
-    private final AddIndexEntryUi addIndexEntryUi;
+    private final AddIndexEntrySwingUi addIndexEntryUi;
 
     public TrayUI(Image image, UiComponent component) {
         super(image);
@@ -35,14 +34,15 @@ public class TrayUI extends TrayIcon implements PropertyChangeListener {
 
         searchFrame = new JFrame("Search");
         searchFrame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
-        searchUi = new SearchUi(component);
-        searchFrame.setContentPane(new SearchUi(component).root);
+        searchUi = new SearchSwingUi(searchFrame, false, component);
+//        searchFrame.setContentPane(new SearchSwingUi(searchFrame, true, component).root);
+        searchFrame.setContentPane(searchUi.getRoot());
         searchFrame.pack();
 
         addEntryFrame = new JFrame("Add Index Entry");
-        addIndexEntryUi = new AddIndexEntryUi(component);
+        addIndexEntryUi = new AddIndexEntrySwingUi(addEntryFrame, true, component);
         addEntryFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        addEntryFrame.setContentPane(addIndexEntryUi.root);
+        addEntryFrame.setContentPane(addIndexEntryUi.getRoot());
         addEntryFrame.pack();
 
         EventQueue.invokeLater(new Runnable() {
@@ -51,6 +51,7 @@ public class TrayUI extends TrayIcon implements PropertyChangeListener {
                 createGui();
             }
         });
+
     }
 
     private void createGui() {
@@ -66,11 +67,24 @@ public class TrayUI extends TrayIcon implements PropertyChangeListener {
         popup.add(addEntry);
         popup.add(exitItem);
         this.setPopupMenu(popup);
+    
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (!SwingUtilities.isRightMouseButton(e)) {
+                    searchFrame.setVisible(true);
+                }
+            }
+        }
+        );
+        
         try {
             SystemTray.getSystemTray().add(this);
         } catch (AWTException e) {
             e.printStackTrace();
         }
+
+    
     }
 
     private ActionListener getSearchAction() {
