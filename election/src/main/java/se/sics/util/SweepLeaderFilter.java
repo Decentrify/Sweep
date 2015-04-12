fd7f71a3739277b587a7ee51589f5128e6be7e7e
Abhimanyu Babbar <abhimanyu.babbar@sics.se>
2015-04-12 20:42:06 +0200

    Updated the logic for calculating the point to terminate leader by application.

--- election/src/main/java/se/sics/util/SweepLeaderFilter.java
+++ election/src/main/java/se/sics/util/SweepLeaderFilter.java
@@ -1,5 +1,6 @@
 package se.sics.util;
 
+import se.sics.ms.types.SearchDescriptor;
 import se.sics.p2ptoolbox.election.api.LCPeerView;
 import se.sics.p2ptoolbox.election.core.util.LeaderFilter;
 
@@ -12,6 +13,18 @@ public class SweepLeaderFilter implements LeaderFilter{
 
     @Override
     public boolean terminateLeader(LCPeerView old, LCPeerView updated) {
-        return false;
+
+        SearchDescriptor previous;
+        SearchDescriptor current;
+
+        if( !(old instanceof SearchDescriptor) || !(updated instanceof SearchDescriptor)){
+            throw new IllegalArgumentException("Unknown types of arguments.");
+        }
+        
+        previous = (SearchDescriptor)old;
+        current = (SearchDescriptor)updated;
+        
+        // Return true in event of increase in partitioning depth.
+        return (current.getPartitioningDepth() > previous.getPartitioningDepth());
     }
 }
